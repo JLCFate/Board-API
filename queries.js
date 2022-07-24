a1ccf9bac61fcd1a110951ab74187619b4591cf5
JLCFate <grzgra88@gmail.com>
2022-07-24 19:41:14 +0200

    [STRUCTURE] Added REST namespaces

--- queries.js
+++ queries.js
@@ -7,14 +7,27 @@ const pool = new Pool({
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 })
+const queryAll = (response, status) => {
+  pool.query('SELECT * FROM users ', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(status).json(results.rows)
+  })
+}
+
+
+const getLogs = (request, response) => {
+  pool.query('SELECT * FROM logs ', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
+    queryAll(response, 200)
   }
 
   const createUser = (request, response) => {
@@ -24,33 +37,34 @@ const getUsers = (request, response) => {
       if (error) {
         throw error
       }
-      response.status(201).send(`User added`)
+      queryAll(response, 201)
     })
   }  
 
   const updateUser = (request, response) => {
     const { name, address } = request.body
+    const Oldaddress = request.params.address
   
     pool.query(
       'UPDATE users SET name = $1, address = $2 WHERE address = $3',
-      [name, address],
+      [name, address, Oldaddress],
       (error, results) => {
         if (error) {
           throw error
         }
-        response.status(200).send(`User modified`)
+        queryAll(response, 200)
       }
     )
   }
 
   const deleteUser = (request, response) => {
-    const address = parseInt(request.params.address)
+    const address = request.params.address
   
     pool.query('DELETE FROM users WHERE address = $1', [address], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`User deleted`)
+      queryAll(response, 200)
     })
   }  
   module.exports = {
@@ -58,4 +72,5 @@ const getUsers = (request, response) => {
     createUser,
     updateUser,
     deleteUser,
+    getLogs,
   }
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const db = require('./queries')
 const port = 3000
+
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+    'DELETE',
+    'PUT'
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use(cors(corsOpts))
 
 app.use(bodyParser.json())
 app.use(
@@ -14,6 +31,7 @@ app.get('/users', db.getUsers)
 app.post('/users', db.createUser)
 app.put('/users/:address', db.updateUser)
 app.delete('/users/:address', db.deleteUser)
+app.get('/logs', db.getLogs)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
