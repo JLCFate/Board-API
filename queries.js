2b8205bebb18b2ca25d5704c7695f9694dc76e1a
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-08-01 14:20:06 +0200

    [Authentication] New endpoint to handle auth

--- queries.js
+++ queries.js
@@ -1,90 +1,98 @@
-require('dotenv').config();
-const Pool = require('pg').Pool
+require("dotenv").config();
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: process.env.POSTGRES_USERNAME,
-  host: process.env.POSTGRES_HOST,
-  database: 'mac_users',
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-})
+	user: process.env.POSTGRES_USERNAME,
+	host: process.env.POSTGRES_HOST,
+	database: "mac_users",
+	password: process.env.POSTGRES_PASSWORD,
+	port: 5432,
+});
 
 const queryAll = (response, status) => {
-  pool.query('SELECT * FROM users ', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(status).json(results.rows)
-  })
-}
-
+	pool.query("SELECT * FROM users ", (error, results) => {
+		if (error) {
+			throw error;
+		}
+		response.status(status).json(results.rows);
+	});
+};
 
 const getLogs = (request, response) => {
-  pool.query('SELECT * FROM logs ', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+	pool.query("SELECT * FROM logs ", (error, results) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).json(results.rows);
+	});
+};
 
 const getUsers = (request, response) => {
-    queryAll(response, 200)
-  }
+	queryAll(response, 200);
+};
 
-  const createUser = (request, response) => {
-    const { name, address } = request.body
-  
-    pool.query('INSERT INTO users (name, address) VALUES ($1, $2) RETURNING *', [name, address], (error, results) => {
-      if (error) {
-        throw error
-      }
-      queryAll(response, 201)
-    })
-  }  
+const createUser = (request, response) => {
+	const { name, address } = request.body;
 
-  const updateUser = (request, response) => {
-    const { name, address } = request.body
-    const Oldaddress = request.params.address
-  
-    pool.query(
-      'UPDATE users SET name = $1, address = $2 WHERE address = $3',
-      [name, address, Oldaddress],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        queryAll(response, 200)
-      }
-    )
-  }
+	pool.query("INSERT INTO users (name, address) VALUES ($1, $2) RETURNING *", [name, address], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		queryAll(response, 201);
+	});
+};
 
-  const deleteUser = (request, response) => {
-    const address = request.params.address
-  
-    pool.query('DELETE FROM users WHERE address = $1', [address], (error, results) => {
-      if (error) {
-        throw error
-      }
-      queryAll(response, 200)
-    })
-  }  
+const updateUser = (request, response) => {
+	const { name, address } = request.body;
+	const Oldaddress = request.params.address;
 
-  const checkUsers = (request, response) => {
-    const Oldaddress = request.params.address
-    pool.query('SELECT * FROM users WHERE address =$1', [Oldaddress], (error, results) => {
-      if (error) {
-        throw error
-      }
-      const status = results.rows.length > 0 ? 200 : 400
-      response.status(status).json(results.rows)
-    })
-  }
-  
-  module.exports = {
-    getUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-    getLogs,
-    checkUsers,
-  }
+	pool.query("UPDATE users SET name = $1, address = $2 WHERE address = $3", [name, address, Oldaddress], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		queryAll(response, 200);
+	});
+};
+
+const updateAuthorized = (requrest, response) => {
+	const { authorized, awaiting } = requrest.body;
+	const address = requrest.params.address;
+
+	pool.query("UPDATE users SET authorized = $1, awaiting = $2 WHERE address = $3", [authorized, awaiting, address], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		queryAll(response, 200);
+	});
+};
+
+const deleteUser = (request, response) => {
+	const address = request.params.address;
+
+	pool.query("DELETE FROM users WHERE address = $1", [address], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		queryAll(response, 200);
+	});
+};
+
+const checkUsers = (request, response) => {
+	const Oldaddress = request.params.address;
+	pool.query("SELECT * FROM users WHERE address =$1", [Oldaddress], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		const status = results.rows.length > 0 ? 200 : 400;
+		response.status(status).json(results.rows);
+	});
+};
+
+module.exports = {
+	getUsers,
+	createUser,
+	updateUser,
+	deleteUser,
+	getLogs,
+	checkUsers,
+	updateAuthorized,
+};
--- server.js
+++ server.js
@@ -1,40 +1,33 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const db = require("./queries");
+const port = 4000;
 
 const corsOpts = {
-  origin: '*',
+	origin: "*",
 
-  methods: [
-    'GET',
-    'POST',
-    'DELETE',
-    'PUT'
-  ],
+	methods: ["GET", "POST", "DELETE", "PUT"],
 
-  allowedHeaders: [
-    'Content-Type',
-  ],
+	allowedHeaders: ["Content-Type"],
 };
-app.use(cors(corsOpts))
+app.use(cors(corsOpts));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-app.get('/users', db.getUsers)
-app.post('/users', db.createUser)
-app.put('/users/:address', db.updateUser)
-app.delete('/users/:address', db.deleteUser)
-app.get('/logs', db.getLogs)
-app.get('/users/:address', db.checkUsers)
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.get("/users", db.getUsers);
+app.post("/users", db.createUser);
+app.put("/users/:address", db.updateUser);
+app.delete("/users/:address", db.deleteUser);
+app.get("/logs", db.getLogs);
+app.get("/users/:address", db.checkUsers);
+app.put("/users/authenticate/:address", db.updateAuthorized);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
-
+	console.log(`App running on port ${port}.`);
+});
