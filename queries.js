84d1df64ee45c04358066a348fd902d5a8ee7725
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-08-08 20:47:34 +0200

    [Deploy] Heroku

--- queries.js
+++ queries.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-	user: process.env.POSTGRES_USERNAME,
-	host: process.env.POSTGRES_HOST,
-	database: process.env.POSTGRES_DATABASE,
-	password: process.env.POSTGRES_PASSWORD,
-	port: 5432,
-	ssl: true,
+	connectionString: process.env.DATABASE_URL,
+	ssl: { rejectUnauthorized: false },
 });
 
 const queryAll = (response, status) => {
